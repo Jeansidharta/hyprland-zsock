60a6f0158a44bef40d3663088293a414cd6cc68d
sidharta <jeansidharta@gmail.com>
2025-02-17 20:53:41 -0300

    add package to configuration.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,59 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    zig.url = "github:mitchellh/zig-overlay";
+    zig-flake.url = "github:mitchellh/zig-overlay";
   };
   outputs =
     {
       self,
       nixpkgs,
       utils,
-      zig,
+      zig-flake,
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
+        project_name = "hyprland-zsock";
+        version = "0.0.1";
+        deps = [
+          # dep1 = inputs.dep1
+        ];
+
         pkgs = nixpkgs.legacyPackages.${system};
+        zig = zig-flake.outputs.packages.${system}.master;
+        mkLibsLinkScript = ''
+          rm --force libs
+          ln -s ${pkgs.linkFarm (project_name + "-deps") deps} libs
+        '';
+        package = pkgs.stdenv.mkDerivation {
+          pname = project_name;
+          version = version;
+          src = ./.;
+          buildInputs = [
+            zig
+          ];
+
+          buildPhase = ''
+            cp --no-preserve=mode $src/* . -r
+            ${mkLibsLinkScript}
+
+            zig build \
+              --prefix $out \
+              --release=safe \
+              -Doptimize=ReleaseSafe \
+              -Ddynamic-linker=$(cat $NIX_BINTOOLS/nix-support/dynamic-linker) \
+              --cache-dir cache \
+              --global-cache-dir global \
+              --summary all
+          '';
+        };
       in
       {
+        packages.default = package;
         devShell = pkgs.mkShell {
+          shellHook = mkLibsLinkScript;
           buildInputs = [
-            zig.outputs.packages.${system}.master
+            zig
           ];
         };
       }
